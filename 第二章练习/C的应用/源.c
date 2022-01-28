2987207265f706c179675081a078e5da86323238
PangBoy567 <1341797025@qq.com>
2022-01-28 16:03:11 +0800

    第二章练习题

---
+++ 第二章练习/C的应用/源.c
@@ -0,0 +1,145 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#define NAME "Tianxiang"
+//#define SURNAME "Pang"
+//int main(void)
+//{
+//	printf("%s %s\n", NAME, SURNAME);
+//	printf("%s\n%s\n", NAME, SURNAME);
+//	printf("%s ", NAME);
+//	printf("%s\n", SURNAME);
+//
+//	return 0;
+//}
+//#include <stdio.h>
+//int main(void)
+//{
+//	printf("Gustav Mahler\n");
+//	printf("Gustav\nMahler\n");
+//	printf("Gustav ");
+//	printf("Mahler\n");
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#define name "Tianxiang Pang"
+//#define address "我们家的地址"
+//int main(void)
+//{
+//	printf("%s\n", name);
+//	printf("%s", address);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int age = 0;
+//	int days = 0;
+//	printf("请输入你的年龄\n");
+//	scanf("%d", &age);
+//	days = age * 365;
+//	printf("根据您的年龄，您一共经历了%d天", days);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//void jolly(void)
+//{
+//	printf("For he's jolly good fellow!\n");
+//}
+//void deny(void)
+//{
+//	printf("Which nobody can deny!\n");
+//}
+//int main(void)
+//{
+//	jolly();
+//	jolly();
+//	jolly();
+//	deny();
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//void br(void)
+//{
+//	printf("Brazil, Russia, ");
+//}
+//void ic(void)
+//{
+//	printf("India, China\n");
+//}
+//int main(void)
+//{
+//	br();
+//	ic();
+//	ic();
+//	br();
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int toes = 10;
+//	int toes2 = toes * 2;
+//	int toes3 = toes * toes;
+//	printf("当toes=%d时，toes的两倍是%d,toes的平方是%d", toes, toes2, toes3);
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//void smile(void);
+//void smile(void)
+//{
+//	printf("Smile!");
+//}
+//
+//int main(void)
+//{
+//	printf("许多研究表明，微笑益处多多\n");
+//	smile(); smile(); smile();
+//	printf("\n");
+//	smile(); smile();
+//	printf("\n");
+//	smile();
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//void two(void);
+//void one_three(void);
+//void two(void)
+//{
+//	printf("two");
+//}
+//void one_three(void)
+//{
+//	printf("one\n");
+//	two();
+//	printf("\nthree");
+//}
+//int main(void)
+//{
+//	printf("starting now:\n");
+//	one_three();
+//	printf("\ndone!");
+//
+//	return 0;
+//}
+
+
